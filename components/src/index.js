95c81abf99ad5584b4b71bfa61f6ab5f0125e90d
John McKie <john.mckie@remarkable.net>
2019-02-25 11:00:25 +0000

    Creation of the components project

---
+++ components/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
