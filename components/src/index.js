0642c84851c2bbfa3c00c9dde0bbb1c161752192
John McKie <john.mckie@remarkable.net>
2019-02-25 16:05:21 +0000

    Some stuff with props and that

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-            </div>
+
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Derek" 
+                    timeAgo="Today at 6:45AM" 
+                    comment="Nice work man!" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Arnold" 
+                    timeAgo="Today at 08:00PM" 
+                    comment="Hasta la vista etc..!" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Blernard" 
+                    timeAgo="Yesterday at 09:05PM" 
+                    comment="YARP!!" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
         </div>
     );
 };
