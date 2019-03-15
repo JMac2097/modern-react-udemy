742e5631ac81a817613c6a157d837a39dcc7b54d
John McKie <john.mckie@remarkable.net>
2019-03-15 14:58:12 +0000

    setup of the youtube api

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,2 @@
+const KEY = 'AIzaSyBc887ExK-pLMnW_V6aArcJWfCJ0O_M8NY';
+
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import SearchBar from './SearchBar';
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className="ui container">
                 <SearchBar />
             </div>
-        );
+        ) 
+            
+        
     }
 }
 
