46b4f85a9aeb9edffd3988f488143157153ca34c
John McKie <john.mckie@remarkable.net>
2019-03-19 14:38:00 +0000

    some more work on the videos app

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,2 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBc887ExK-pLMnW_V6aArcJWfCJ0O_M8NY';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
 
